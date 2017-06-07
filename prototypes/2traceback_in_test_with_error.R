d3f2675d423a6aaaba42e8853a2b399383004475
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-06-07 13:21:52 +0200

    build a prototype to report traceback for a failing test

--- prototypes/2traceback_in_test_with_error.R
+++ prototypes/2traceback_in_test_with_error.R
@@ -1,30 +1,18 @@
-# The purpose is to get a traceback of an error that occured in a test
-# This is achieved by sys.call() which has to be executed within withCallingHandler
-# since the environment of the execution is not available for the error function
-# if we use tryCatch 
-# On the other hand withCallingHandlers does not recover from the error
-# So we have to catch it wiht a surrounding tryCatch
+# This is a version of traceback_in_test_with_error.R that avoids
 f <- function(x){x^2}
 f1 <- function(x){f2(x)}
 f2 <- function(x){stop('some error')}
 
-execute <- function(){
-		withRestarts(
-			f1(3),
-			report_callStack=function(e){
-				tr <- ""
-				withCallingHandlers(
-					f1(3),
-					error=function(e){
-						tr <<- sys.calls()
-					}
-				)
-				return(tr)
-			}
-		)
+execOrReport <- function( func){
+	tryCatch(
+		withCallingHandlers(
+				error = function(e){
+					r<<-sys.calls()
+				},
+				r <- func(3)
+		),
+		error=function(e){list(e,r)}
+	)
 }
-res <- withCallingHandlers(
-	execute(),
-	error=function(){invokeRestart("report_callStack")},
-)
-print(res)
+print(execOrReport(f1))
+print(execOrReport(f))
