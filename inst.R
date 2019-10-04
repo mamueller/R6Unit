3a6b2d11fbf4a546ac44f3a65439d6f137db7192
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-10-04 16:50:36 +0200

    had forgotten to install getopt()

--- inst.R
+++ inst.R
@@ -3,6 +3,7 @@ if (!is.element('devtools',installed.packages())){
 	install.packages('devtools',repos='https://cran.uni-muenster.de')
 }
 install.packages('stringr')
+install.packages('getopt')
 require(getopt)
 require(stringr)
 require(devtools)
