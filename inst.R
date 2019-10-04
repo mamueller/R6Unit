99495b3cb3639c5c1e6959247bf22207911e4464
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-10-04 04:50:04 +0200

    corrected order of installation

--- inst.R
+++ inst.R
@@ -2,10 +2,10 @@
 if (!is.element('devtools',installed.packages())){
 	install.packages('devtools',repos='https://cran.uni-muenster.de')
 }
-require(devtools)
 install.packages('stringr')
-devtools::install('pkg',build=FALSE)
-devtools::install_github('mamueller/debugHelpers',subdir='pkg')
 require(getopt)
 require(stringr)
+require(devtools)
+devtools::install_github('mamueller/debugHelpers',subdir='pkg')
+devtools::install('pkg',build=FALSE)
 print(get_Rscript_filename())
